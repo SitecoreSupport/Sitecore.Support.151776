3bec645f4106926de8963fbce9b057c1eacb35a5
ido <ido@WS-IDO1UA1>
2017-03-15 14:33:12 +0200

    First commit

---
+++ src/Sitecore.Support.151776/Pipelines/ItemProvider/GetItem/DisableEnforcingVersionPresenceForExperienceEditor.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Pipelines.ItemProvider.GetItem;
+
+namespace Sitecore.Support.Pipelines.ItemProvider.GetItem
+{
+  public class DisableEnforcingVersionPresenceForExperienceEditor : GetItemProcessor
+  {
+    public override void Process(GetItemArgs args)
+    {
+      if (Sitecore.Context.Site != null && Sitecore.Context.PageMode.IsExperienceEditor)
+      {
+        if (args != null && args.Result == null && args.Language != null && args.Version != null && args.Database != null)
+        {
+          Item item = null;
+          if ((object)args.ItemId != null)
+          {
+            item = args.FallbackProvider.GetItem(args.ItemId, args.Language, args.Version, args.Database, args.SecurityCheck);
+          }
+          else if (!string.IsNullOrWhiteSpace(args.ItemPath))
+          {
+            item = args.FallbackProvider.GetItem(args.ItemPath, args.Language, args.Version, args.Database, args.SecurityCheck);
+          }
+          args.Result = item;
+        }
+      }      
+    }
+  }
+}
